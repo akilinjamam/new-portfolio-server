9a97ba30b92505a32519cff2ebcacd2ff5c63e49
akilinjamam <akilinjamam@gmail.com>
2024-08-29 23:39:53 -0500

    login-updated

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ app.use(
   cors({
     origin: [
       'https://level-2-24-assignment-5-client.vercel.app',
-      'http://localhost:5173/',
+      'http://localhost:5173',
     ],
     credentials: true,
   }),
