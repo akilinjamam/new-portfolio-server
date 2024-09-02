1ec427ec1628502b712b1f05388f17380645e29a
akilinjamam <akilinjamam@gmail.com>
2024-09-01 20:49:45 -0500

    file-updated

--- src/modules/payment/payment.controller.ts
+++ src/modules/payment/payment.controller.ts
@@ -1,12 +1,12 @@
-import { StatusCodes } from 'http-status-codes';
+// import { StatusCodes } from 'http-status-codes';
 import catchAsync from '../../app/utils/catchAsync';
-import sendRespone from '../../app/utils/sendRespone';
+// import sendRespone from '../../app/utils/sendRespone';
 import { paymentSevice } from './payment.service';
 
 const createSuccess = catchAsync(async (req, res) => {
   const { bookingId } = req.query;
 
-  const result = await paymentSevice.createSuccess(bookingId as string);
+  await paymentSevice.createSuccess(bookingId as string);
 
   res.redirect('https://level-2-24-assignment-5-client.vercel.app/success');
 
@@ -17,12 +17,12 @@ const createSuccess = catchAsync(async (req, res) => {
   //   );
   // }
 
-  sendRespone(res, {
-    success: true,
-    statusCode: StatusCodes.OK,
-    message: 'Payment updated successfully',
-    data: result,
-  });
+  // sendRespone(res, {
+  //   success: true,
+  //   statusCode: StatusCodes.OK,
+  //   message: 'Payment updated successfully',
+  //   data: result,
+  // });
 });
 
 export const paymentController = {
